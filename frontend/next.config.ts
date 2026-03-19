0ddeac7564ea5f90dd2d7998f870c698c9589eaf
jewronimo <jostashevskiy@gmail.com>
2026-03-19 12:12:00 -0700

    Proper image display in gallery

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["https://192.168.1.119"],
+  allowedDevOrigins: ["192.168.1.119"],
   async rewrites() {
     return [
       {
